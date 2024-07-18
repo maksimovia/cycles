70ae6cd1a55834fd6a8c49c8a63879e817d549b0
Igor Maksimov <st.igoriy@gmail.com>
2024-07-18 23:43:53 +0300

    1807

--- ORCregen.py
+++ ORCregen.py
@@ -1,24 +1,24 @@
 from CoolProp.CoolProp import PropsSI as prop
 from data import nodes, blocks
-from modules import comp, heat
-
+from modules import comp, heat, turb, cond
+from scipy.optimize import root_scalar
 
 # Исходные данные блоков
-Ppump = 4e6
+Ppump = 6e6
 KPDpump = 0.8
 KPDturb = 0.9
-dTh = 10
+dTh = 20
 dTr = 5
 
 # Ввод исходных данных в (3)
 X3 = 'R236ea'
-G3 = 9999
-P3 = 245000
-T3 = prop("T", "P", P3, "Q", 0, X3)
-H3 = prop("H", "P", P3, "Q", 0, X3)
-S3 = prop("S", "P", P3, "Q", 0, X3)
-Q3 = prop("Q", "P", P3, "Q", 0, X3)
-nodes.loc['3'] = [T3, P3, H3, S3, Q3, G3, X3]
+G3 = 300
+Pcond = 245000
+T3 = prop("T", "P", Pcond, "Q", 0, X3)
+H3 = prop("H", "P", Pcond, "Q", 0, X3)
+S3 = prop("S", "P", Pcond, "Q", 0, X3)
+Q3 = prop("Q", "P", Pcond, "Q", 0, X3)
+nodes.loc['3'] = [T3, Pcond, H3, S3, Q3, G3, X3]
 
 # Ввод исходных данных в (1)
 X1 = 'REFPROP::N2[0.77]&O2[0.14]&H2O[0.06]&CO2[0.03]'
@@ -31,13 +31,39 @@ Q1 = prop("Q", "P", P1, "T", T1, X1)
 nodes.loc['1'] = [T1, P1, H1, S1, Q1, G1, X1]
 Tout = 80+273.15
 
-i=0
 
-comp('PUMP', '3', '4', Ppump, KPDpump)
-if i == 0:
-    nodes.loc['5'] = nodes.loc['4']
-else:
-    print(0)
-heat('HEATER', '1', '2', '5', '6', Tout, dTh)
+for i in range(9999):
+    comp('PUMP', '3', '4', Ppump, KPDpump)
+    if i == 0:
+        nodes.loc['5'] = nodes.loc['4']
+    else:
+        def Groot1(T8):
+            heat('REGEN', '7', '8', '4', '5', T8)
+            return dTr - blocks.loc['REGEN', 'dT']
+        root_scalar(Groot1, bracket=[nodes.loc['4', 'T'], nodes.loc['7', 'T']], xtol=10 ** -5)
 
-print(nodes.loc[:,"T":"G"])
+    def Groot2(G5):
+        nodes.loc['5', 'G'] = G5
+        heat('HEAT', '1', '2', '5', '6', Tout)
+        return dTh - blocks.loc['HEAT', 'dT']
+    root_scalar(Groot2, x0=nodes.loc['5', 'G'], xtol=10**-5)
+
+    turb('TURB', '6', '7', Pcond, KPDturb)
+
+    def Groot3(T8):
+        heat('REGEN', '7', '8', '4', '5', T8)
+        return dTr - blocks.loc['REGEN', 'dT']
+    root_scalar(Groot3, bracket=[nodes.loc['4', 'T'], nodes.loc['7', 'T']], xtol=10**-5)
+
+    cond('COND', '8','3')
+
+    balance = abs(blocks.loc['HEAT','Q']+blocks.loc['PUMP','N']-blocks.loc['COND','Q']-blocks.loc['TURB','N'])/blocks.loc['HEAT','Q']
+    print(balance)
+    if balance < 10**-5:
+        break
+
+KPD = (blocks.loc['TURB','N']-blocks.loc['PUMP','N'])/blocks.loc['HEAT','Q']
+print(KPD)
+print(nodes.loc[:, "T":"G"])
+
+
--- modules.py
+++ modules.py
@@ -19,69 +19,73 @@ def comp(name, node1, node2, P2, eff):
     pass
 
 
-def heat(name, node11, node12, node21, node22, T12, dT):
-    n = 51
-
+def heat(name, node11, node12, node21, node22, T12):
+    n = 10
     fluid1 = nodes.loc[node11]['fluid']
     fluid2 = nodes.loc[node21]['fluid']
-
     H11 = nodes.loc[node11]['H']
     T11 = nodes.loc[node11]['T']
     P11 = nodes.loc[node11]['P']
     P21 = nodes.loc[node21]['P']
-
+    T21 = nodes.loc[node21]['T']
     G1 = nodes.loc[node11]['G']
     H12 = prop("H", "T", T12, "P", P11, fluid1)
     G2 = nodes.loc[node21]['G']
     H21 = nodes.loc[node21]['H']
-
     step = (H11 - H12) / (n-1)
     t1 = np.zeros(n)
     t2 = np.zeros(n)
     Q = np.zeros(n)
-    print(step)
     h11 = H11
     h21 = H21
     t1[0] = T11
+    t2[-1] = T21
     for i in range(n-1):
         h12 = h11 - step
         t1[i+1] = prop('T', 'H', h12, 'P', P11, fluid1)
         Q[i+1] = Q[i] + G1*(h11-h12)
-        print(i)
+        h11 = h12
     for i in range(n-1):
-        h22 = h21 + (Q[n-1 - i] - Q[n-1 - i - 1])/G2
-        t2[n-1-i] = prop('T', 'H', h22, 'P', P21, fluid2)
+        h22 = h21 + (Q[-1-i] - Q[-2-i])/G2
+        t2[-2-i] = prop('T', 'H', h22, 'P', P21, fluid2)
+        h21 = h22
+    dT = t1 - t2
+    mindT = min(dT)
+    T22 = t2[0]
+    H22 = h22
+    S12 = prop('S', 'H', H12, 'P', P11, fluid1)
+    Q12 = prop('Q', 'H', H12, 'P', P11, fluid1)
+    S22 = prop('S', 'H', H22, 'P', P21, fluid2)
+    Q22 = prop('Q', 'H', H22, 'P', P21, fluid2)
+    nodes.loc[node12] = [T12, P11, H12, S12, Q12, G1, fluid1]
+    nodes.loc[node22] = [T22, P21, H22, S22, Q22, G2, fluid2]
+    blocks.loc[name, 'Q'] = Q[-1]
+    blocks.loc[name, 'dT'] = mindT
+    pass
 
 
+def turb(name, node1, node2, P2, eff):
+    fluid = nodes.loc[node1]['fluid']
+    S1 = nodes.loc[node1]['S']
+    H1 = nodes.loc[node1]['H']
+    G = nodes.loc[node1]['G']
+    H2t = prop("H", "P", P2, "S", S1, fluid)
+    H2 = H1 - (H1 - H2t)*eff
+    T2 = prop('T', 'H', H2, 'P', P2, fluid)
+    S2 = prop('S', 'H', H2, 'P', P2, fluid)
+    Q2 = prop('Q', 'H', H2, 'P', P2, fluid)
+    nodes.loc[node2] = [T2, P2, H2, S2, Q2, G, fluid]
+    blocks.loc[name, 'N'] = G * (H1 - H2)
+    pass
 
-
-#         step = (self.H11-self.H12)/self.hsteps
-#         t1 = np.zeros(self.hsteps + 1)
-#         t2 = np.zeros(self.hsteps + 1)
-#         Q = np.zeros(self.hsteps + 1)
-#         h11 = self.H11
-#         h21 = self.H21
-#         for i in range(self.hsteps + 1):
-#             t1[i] = prop.h_p(h11, self.P11, self.fluid1)["T"]
-#             if i < self.hsteps:
-#                 h12 = h11 - step
-#                 dQ = self.G1 * (h11 - h12)
-#                 h11 = h12
-#                 Q[i + 1] = Q[i] + dQ
-#         for i in range(self.hsteps + 1):
-#             t2[self.hsteps - i] = prop.h_p(h21, p2, self.fluid2)["T"]
-#             if i < self.hsteps:
-#                 h22 = h21 + (Q[self.hsteps - i] - Q[self.hsteps - i - 1]) * self.dQ / G2
-#                 h21 = h22
-#                 p2 = p2 - dP2 / (self.hsteps)
-#         DT = t1 - t2
-#         min_dt = min(DT[:-1])
-#         T12 = t1[-1]
-#         S12 = prop.h_p(h12, self.P11, self.fluid1)["S"]
-#         Q12 = prop.h_p(h12, self.P11, self.fluid1)["Q"]
-#         S22 = prop.h_p(h22, p2, self.fluid2)["S"]
-#         Q22 = prop.h_p(h22, p2, self.fluid2)["Q"]
-#         self.streams.loc[self.stream12] = [T12, self.P11, h12, S12, Q12, self.G1, self.fluid1]
-#         self.streams.loc[self.stream22] = [t2[0], p2, h22, S22, Q22, G2, self.fluid2]
-#         self.blocks.loc['HEAT'] = [0, Q[-1], min_dt, t1, t2, Q]
-#         pass
+def cond(name, node1, node2):
+    P = nodes.loc[node1]['P']
+    H1 = nodes.loc[node1]['H']
+    fluid = nodes.loc[node1]['fluid']
+    G = nodes.loc[node1]['G']
+    T2 = prop('T', 'Q', 0, 'P', P, fluid)
+    H2 = prop('H', 'Q', 0, 'P', P, fluid)
+    S2 = prop('S', 'Q', 0, 'P', P, fluid)
+    nodes.loc[node2] = [T2, P, H2, S2, 0, G, fluid]
+    blocks.loc[name, 'Q'] = G*(H1 - H2)
+    pass
