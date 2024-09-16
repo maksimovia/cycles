6d994fa366aa695d9aa651983599807ee55ad5a5
PatorkinDV <danilpator02@gmail.com>
2024-09-16 11:59:29 +0300

    3107

--- BraytonRecomp with dP.py
+++ BraytonRecomp with dP.py
@@ -7,6 +7,7 @@ from scipy.optimize import root_scalar,root,minimize
 def Sensitivity(inp):
     P1 = inp[0]
     x = inp[1]
+    dP = 100e3
     #x = 0.68
     G1 = 1
     P7 = 8e6
@@ -19,21 +20,21 @@ def Sensitivity(inp):
     dTl = 10
     def Calc(input):
         T1 = input[0]
-        Node('1',G=G1,P=P1,T=T1,fluid='CO2')
-        heat_source('HEAT', '1', '2', T=T2)
-        turb('TURB','2','3',P7,KPDturb)
+        Node('1',G=G1,P=P1-2*dP,T=T1,fluid='CO2')
+        heat_source('HEAT', '1', '2', T=T2,dP=dP)
+        turb('TURB','2','3',P7+3*dP,KPDturb)
         T12 = input[1]
-        Node('12',G=G1,P=P1,T=T12,fluid='CO2')
-        heat_exch('HTHE', '3', '4', '12', '1', T22=T1)
+        Node('12',G=G1,P=P1-dP,T=T12,fluid='CO2')
+        heat_exch('HTHE', '3', '4', '12', '1', T22=T1,dP1=dP,dP2=dP)
         T5 = input[2]
-        Node('5',G=G1,P=P7,T=T5,fluid='CO2')
+        Node('5',G=G1,P=P7+dP,T=T5,fluid='CO2')
         T8 = input[3]
         Node('8',G=G1*x,P=P1,T=T8,fluid='CO2')
-        heat_exch('LTHE', '4', '5', '8', '9', T12=T5)
+        heat_exch('LTHE', '4', '5', '8', '9', T12=T5,dP1=dP,dP2=dP)
         split('SPLIT','5','6','10',x)
-        heat_source('COOL', '6', '7', T=T7)
+        heat_source('COOL', '6', '7', T=T7,dP=dP)
         comp('MCOMP','7','8',P1,KPDcomp)
-        comp('ACOMP','10','11',P1,KPDcomp)
+        comp('ACOMP','10','11',P1-dP,KPDcomp)
         mix('MIX','9','11','12')
         Equation1 = nodes.loc['8','T'] - T8
         Equation2 = nodes.loc['12','T'] - T12
@@ -49,6 +50,7 @@ def Sensitivity(inp):
     KPD1 = (N_TURB - N_MCOMP - N_RCOMP)/Q_HEAT*100
     print(Q_HEAT + N_MCOMP + N_RCOMP - N_TURB - Q_COND)
     print(P1/1e6,x,KPD1)
+    print(nodes)
 
     return -KPD1
 
